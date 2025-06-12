78f37480c68003f9b764107ef2d1b40d323f8a0b
mohsen <58786220+mt4030@users.noreply.github.com>
2025-06-12 14:41:14 +0330

    Add files via upload

---
+++ sunnyside-agency/app.js
@@ -0,0 +1,10 @@
+
+const btn=document.querySelector('.btn-mobile-nav')
+const list=document.querySelector('.links')
+
+
+btn.addEventListener('click',()=>{
+   list.classList.toggle('nav-open')
+
+
+})
